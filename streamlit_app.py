33569ecb3a208dd63bd748a1b479218bab5cbb56
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-18 20:58:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ if file is not None:
  num_row=st.slider('choose num of row',min_value=1,max_value=len(df),step=1)
  coloums=st.muliselect('choose num of name of coloums',df.coloums.to_list())
  st.write(df[:num_row][coloums])
- st.write(df[:num_row])
+
  
  
 

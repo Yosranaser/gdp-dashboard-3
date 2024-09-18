08e40e8d0a0e81fdf37088cc0d8509e09cb4bfa8
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-18 20:29:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,9 @@
 import streamlit as st
-import time
-area=None
-st.sidebar.title('sidebar')
-st.header('calculate area')
-with st.sidebar:
- choose=st.selectbox('choose box',['circle','trangle'])
-if choose=='circle' :
-  r=st.number_input('enter radius',min_value=1,max_value=100)
-  area=3.14*r*r
-elif choose=='trangle' :
-  hight=st.number_input('enter hight',min_value=1,max_value=100)
-  width=st.number_input('enter width',min_value=1,max_value=100)
-  area=hight*width
-
-btn=st.button('cal')
-if btn:
-  with st.spinner('loadind'):
-   time.sleep(2)
-  st.write(f'area:{area}')
+import pandas as pd
+st.header('file uplaod')
+file=st.file_uploader('upload dataset',type=['csv'])
+if file is not None:
+ df=pd.read_csv(file)
+ st.write(df)
+ 
 

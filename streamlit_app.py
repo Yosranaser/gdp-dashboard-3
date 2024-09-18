19f185314e95511568d3908afacdc3fd42afb295
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-18 20:57:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ file=st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
  df=pd.read_csv(file)
  num_row=st.slider('choose num of row',min_value=1,max_value=len(df),step=1)
+ coloums=st.muliselect('choose num of name of coloums',df.coloums.to_list())
+ st.write(df[:num_row][coloums])
  st.write(df[:num_row])
  
  

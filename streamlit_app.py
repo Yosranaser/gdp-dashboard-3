f49ff23cb11385346c70d09012dde87278eaa694
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-18 20:00:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,16 @@
 import streamlit as st
-st.title("model")
+area=None
+st.header('calculate area')
+choose=st.selectbox('choose box',['circle','trangle'])
+if choose=='circle' :
+  r=st.number_input('enter radius',min_value=1,max_value=100)
+  area=3.14*r*r
+elif choose=='trangle' :
+  hight=st.number_input('enter hight',min_value=1,max_value=100)
+  width=st.number_input('enter width',min_value=1,max_value=100)
+  area=3.14*hight*width
+
+btn=st.button('cal')
+if btn:
+  st.write(f'area{area})
+

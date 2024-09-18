44348677e6ee25a6addccd3c637de4acab2dba3f
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-18 22:11:39 +0300

    Create calulator.py

---
+++ pages/calulator.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import time
+area=None
+st.sidebar.title('sidebar')
+st.header('calculate area')
+with st.sidebar:
+ choose=st.selectbox('choose box',['circle','trangle'])
+if choose=='circle' :
+  r=st.number_input('enter radius',min_value=1,max_value=100)
+  area=3.14*r*r
+elif choose=='trangle' :
+  hight=st.number_input('enter hight',min_value=1,max_value=100)
+  width=st.number_input('enter width',min_value=1,max_value=100)
+  area=hight*width
+
+btn=st.button('cal')
+if btn:
+  with st.spinner('loadind'):
+   time.sleep(2)
+  st.write(f'area:{area}')

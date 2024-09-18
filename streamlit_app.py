207960c061697d2595ca104f5540b993e9b765f6
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-18 20:24:06 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ area=None
 st.sidebar.title('sidebar')
 st.header('calculate area')
 with st.sidebar:
-choose=st.selectbox('choose box',['circle','trangle'])
+ choose=st.selectbox('choose box',['circle','trangle'])
 if choose=='circle' :
   r=st.number_input('enter radius',min_value=1,max_value=100)
   area=3.14*r*r

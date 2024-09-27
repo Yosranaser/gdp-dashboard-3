7e85c19804095fdb43448e0d9e762a3b47823ba8
Yosra Naser <153253079+Yosranaser@users.noreply.github.com>
2024-09-27 18:07:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,6 @@ import pandas as pd
 from sklearn.cluster import KMeans
 import sklearn
 
-# Print the scikit-learn version for debugging
-st.write(f"scikit-learn version: {sklearn.__version__}")
-
-# Function to load the KMeans model from a file with error handling
-def load_model():
-    try:
-        with open('kmeans_model.pkl', 'rb') as file:
-            model = pickle.load(file)
-        return model
-    except FileNotFoundError:
-        st.error("Model file not found. Make sure the 'kmeans_model.pkl' file is in the correct directory.")
-        return None
-    except Exception as e:
-        st.error(f"An error occurred while loading the model: {e}")
-        return None
 
 # Streamlit interface
 st.title("KMeans Model Predictor")
